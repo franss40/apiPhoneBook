6d4ae0b862bb36438d4c784a712c9560dd7c76ec
franss <franss40@gmail.com>
2024-02-25 20:44:41 +0100

    exercises 3.4, part 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -34,7 +34,7 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const findPerson = persons.find(person => person.id === id)
   if (findPerson) {
@@ -42,6 +42,12 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end() 
 })
 
 const PORT = 3001
