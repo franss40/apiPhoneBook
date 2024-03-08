fe4cf10afdc80bbe21ac35c947ba22d923e66665
franss <franss40@gmail.com>
2024-03-08 12:16:50 +0100

    exercises 3.18, part 5 and 6

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.static("dist"))
 
 require("dotenv").config()
 const cors = require("cors")
+const { SchemaTypes } = require("mongoose")
 app.use(cors())
 
 app.use(express.json())
@@ -68,7 +69,7 @@ app.delete("/api/persons/:id", (req, res, next) => {
     })
 })
 
-app.post("/api/persons/", (req, res) => {
+app.post("/api/persons/", (req, res, next) => {
   const body = req.body
 
   const istherename = body.hasOwnProperty("name")
@@ -80,18 +81,60 @@ app.post("/api/persons/", (req, res) => {
   if (!body.name || !body.number) {
     return res.status(400).json({ error: "name or number missing" })
   }
-  /* const findName = persons.find((person) => person.name === body.name)
-  if (findName) {
-    return res.status(400).json({ error: "name must be unique" })
-  } */
 
-  const person = new Person({
+  Person.findOne({name: body.name})
+    .then(result => {
+      if (!result) {
+        const person = new Person({
+          name: body.name,
+          number: body.number,
+        })
+        person.save().then((result) => {
+          res.json(result)
+        })
+      } else {
+        const person = {
+          name: body.name,
+          number: body.number,
+        }
+        const id = result._id.toString()
+        console.log('id', id)
+        Person.findByIdAndUpdate(id, person, { new: true })
+          .then((result2) => {
+            res.json(result2)
+          })
+          .catch((error) => next(error))
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })
+})
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body
+  const id = req.params.id
+  const istherename = body.hasOwnProperty("name")
+  const istherenumber = body.hasOwnProperty("number")
+
+  if (!istherename || !istherenumber) {
+    return res.status(400).json({ error: "missing body" })
+  }
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "name or number missing" })
+  }
+
+  const person = {
     name: body.name,
-    number: body.number
-  })
-  person.save().then(result => {
-    res.json(result)
-  })
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((result2) => {
+      res.json(result2)
+    })
+    .catch((error) => next(error))
 })
 
 const unkKnownEndPoint = (req, res) => {
